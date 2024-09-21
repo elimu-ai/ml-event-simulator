e66900cdc850d4d44e1e36071d20caa3ca4151e1
jo-elimu <1451036+jo-elimu@users.noreply.github.com>
2024-09-21 11:51:22 +0700

    feat: simulate video learning events
    Resolves #3

--- simulate-video-learning-events.py
+++ simulate-video-learning-events.py
@@ -1,5 +1,8 @@
+from datetime import datetime
+import os
 from os.path import basename
 import pandas
+import random
 
 language_codes = ['ENG', 'HIN', 'TGL']
 print(basename(__file__), f'language_codes: {language_codes}')
@@ -12,12 +15,23 @@ print(basename(__file__), f'android_ids: {android_ids}')
 analytics_version_code = 3001018
 print(basename(__file__), f'analytics_version_code: {analytics_version_code}')
 
+date = datetime.today().strftime('%Y-%m-%d')
+print(basename(__file__), f'date: {date}')
+
 def simulateVideoLearningEvent(android_id):
-    """Simulate a VideoLearningEvent, e.g. a video being opened."""
+    """
+    Simulate a VideoLearningEvent, e.g. a video being opened.
+    
+    Should match the CSV format in https://github.com/elimu-ai/analytics/blob/main/app/src/main/java/ai/elimu/analytics/task/ExportEventsToCsvWorker.java
+    """
 
-    # TODO
+    id = 0
+    time = 0
 
-    return {}
+    return {
+        'id': id,
+        'time': time
+    }
 
 for language_code in language_codes:
     print(basename(__file__))
@@ -38,8 +52,25 @@ for language_code in language_codes:
         print(basename(__file__))
         print(basename(__file__), f'android_id: {android_id}')
         
-        event = simulateVideoLearningEvent(android_id)
-        print(basename(__file__), f'event: {event}')
+        video_learning_events = []
+        random_number_of_events = random.randrange(0, 20)
+        print(basename(__file__), f'random_number_of_events: {random_number_of_events}')
+        for i in range(random_number_of_events):
+            print(basename(__file__))
+            event = simulateVideoLearningEvent(android_id)
+            print(basename(__file__), f'event: {event}')
+            video_learning_events.append(event)
+        
+        video_learning_events_df = pandas.DataFrame(video_learning_events)
+        print(basename(__file__), f'video_learning_events_df: \n{video_learning_events_df}')
 
         # Export to CSV
-        # TODO
+        language_dir = f'lang-{language_code}'
+        android_id_dir = os.path.join(language_dir, f'android-id-{android_id}')
+        version_code_dir = os.path.join(android_id_dir, f'version-code-{analytics_version_code}')
+        storybook_learning_events_dir = os.path.join(version_code_dir, f'storybook-learning-events')
+        if not os.path.exists(storybook_learning_events_dir):
+            os.makedirs(storybook_learning_events_dir)
+        csv_path = os.path.join(storybook_learning_events_dir, f'{android_id}_{analytics_version_code}_storybook-learning-events_{date}.csv')
+        print(basename(__file__), f'csv_path: \n{csv_path}')
+        video_learning_events_df.to_csv(csv_path, index=False)
